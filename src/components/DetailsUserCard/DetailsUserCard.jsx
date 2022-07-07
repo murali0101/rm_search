d8f8f9bd979ea9343a0d851ddb59f1a2afc34a1b
murali0101 <muralimv0101@gmail.com>
2022-07-07 12:31:35 +0530

    Update DetailsUserCard.jsx

--- src/components/DetailsUserCard/DetailsUserCard.jsx
+++ src/components/DetailsUserCard/DetailsUserCard.jsx
@@ -1,5 +1,22 @@
+import { Box, Heading, VStack } from "@chakra-ui/react";
+import { useDispatch, useSelector } from "react-redux";
+const detailsUserCard = useSelector((state) => state.homePage.detailsUserCard);
+console.log('detailsUserCard:', detailsUserCard)
+
 export const DetailsUserCard = ({ props }) => {
-  
-    return <div>DetailsUserCard</div>;
-  };
-  
+  return (
+    <>
+      <Box w={"100%"} h={"100%"} bg="#88888a53" p={1} pos="fixed" zIndex={2} display={false?"block":"none"}>
+        <Box
+          w={400}
+          h={100}
+          bg="#FFFFFF"
+          pos="absolute"
+          top="45%"
+          left="50%"
+          transform={"translate(-50%,-50%)"}
+        ></Box>
+      </Box>
+    </>
+  );
+};
