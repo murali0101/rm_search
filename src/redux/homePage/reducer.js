ee1d605d7681b5710752f1302f4952cb3693451b
murali0101 <muralimv0101@gmail.com>
2022-07-07 09:05:50 +0530

    Update reducer.js

--- src/redux/homePage/reducer.js
+++ src/redux/homePage/reducer.js
@@ -1,9 +1,14 @@
+import { ADD_DATA } from "./actionType";
+
 const init = {
-  data: [],
+    info: {},
+    results: [],
 };
 
-export const homePageReducer = (state = init, { type, action }) => {
-  switch (key) {
+export const homePageReducer = (state = init, { type, payload}) => {
+    switch (type) {
+      case ADD_DATA:{return {...state,results:payload.results,info:payload.info}}
+  
     default:
       return state;
   }
