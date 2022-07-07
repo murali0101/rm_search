9c6b367449b9ad8978de7aea5f31f2db8a4aa3cc
murali0101 <muralimv0101@gmail.com>
2022-07-07 15:56:36 +0530

    Update action.js

--- src/redux/homePage/action.js
+++ src/redux/homePage/action.js
@@ -1,5 +1,11 @@
 import axios from "axios";
-import { ADD_DATA, ADD_DATA_DETAILS_USER_CARD_DATA, ADD_INFO, ADD_RESULTS, DETAILS_USER_CARD } from "./actionType";
+import {
+  ADD_DATA,
+  ADD_DATA_DETAILS_USER_CARD_DATA,
+  ADD_INFO,
+  ADD_RESULTS,
+  DETAILS_USER_CARD,
+} from "./actionType";
 
 // export const addResults = (value) => {
 //   return { type: ADD_RESULTS, payload: value };
@@ -13,15 +19,18 @@ export const addData = (value) => {
 export const detailsUserCard = (value) => {
   return { type: DETAILS_USER_CARD, payload: value };
 };
-export const addDataDetailsUserCardData= (value) => {
+export const addDataDetailsUserCardData = (value) => {
   return { type: ADD_DATA_DETAILS_USER_CARD_DATA, payload: value };
 };
 
-export const getData = (page) => async (dispatch) => {
+export const getData = (value) => async (dispatch) => {
   try {
-    let res = await axios.get("https://rickandmortyapi.com/api/character");
+    let res = await axios.get(
+      `https://rickandmortyapi.com/api/character/?name=Morty&page=1`
+    );
+    // let res = await axios.get(`https://rickandmortyapi.com/api/character/?${value[0]!=null?"name=morty":""}&page=1`);
     dispatch(addData(res.data));
-    
+
     // console.log(res);
   } catch (error) {
     console.log("error:", error);
