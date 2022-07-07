f7bd9eb42f4381e1de94716aada7ff50ecbc6d4b
murali0101 <muralimv0101@gmail.com>
2022-07-07 10:00:52 +0530

    Update BasicUserCard.jsx

--- src/components/BasicUserCard/BasicUserCard.jsx
+++ src/components/BasicUserCard/BasicUserCard.jsx
@@ -1,6 +1,13 @@
-export const BasicUserCard = () => {
-  return <>
-    
-  </>;
-  };
-  
+import { Box, HStack, Avatar ,Text } from "@chakra-ui/react";
+export const BasicUserCard = ({ props }) => {
+  console.log(props);
+  const { name, species, image, status } = props;
+  return (
+    <>
+      <Box bg="#FFFFFF" display={"flex"} maxW={450}>
+        <Avatar name="Dan Abrahmov" src={image} />
+        <Text>{name}</Text>
+      </Box>
+    </>
+  );
+};
