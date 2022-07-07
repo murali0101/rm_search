38d49f8139c252775c1d9c498affd1ede46e0dba
murali0101 <muralimv0101@gmail.com>
2022-07-07 10:01:03 +0530

    Update HomePage.jsx

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ import { getData } from "../../redux/homePage/action";
 import { BasicUserCard } from "../BasicUserCard/BasicUserCard";
 import { SearchBar } from "../SearchBar/SearchBar";
 import { useDispatch, useSelector } from "react-redux";
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, VStack } from "@chakra-ui/react";
 
 export const HomePage = () => {
   const dispatch = useDispatch();
@@ -18,14 +18,22 @@ export const HomePage = () => {
   return (
     <>
       {results ? (
-        <Box maxW={550} m={"auto"} mt={20} textAlign="center" >
-          <Heading as="h6" fontSize={47} letterSpacing={1.5}>
+        <Box maxW={550} m={"auto"} mt={20}>
+          <Heading
+            as="h6"
+            fontSize={47}
+            letterSpacing={1.5}
+            textAlign="center"
+            bg="blue.400"
+          >
             Rick and Morty Search
           </Heading>
           <br />
           <br />
           <SearchBar />
-          <BasicUserCard props={results} />
+          <VStack maxW={450} bg="blackAlpha.300" m={"auto"}  mt={5} textAlign="left">
+            {results.map((ele, ind) => <BasicUserCard props={ ele} key={ ind} />)}
+          </VStack>
         </Box>
       ) : (
         "loading....."
