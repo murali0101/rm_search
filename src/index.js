49d84a4b4ea18790b56746beefb39f5bdc9c496b
murali0101 <muralimv0101@gmail.com>
2022-07-07 09:27:45 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,17 +4,18 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider as ReduxProvider } from "react-redux";
+import { ChakraProvider } from "@chakra-ui/react";
 import { store } from "./redux/store/store";
+import { theme } from "./theme";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ReduxProvider store={store}>
-      <App />
-    </ReduxProvider>
+    <ChakraProvider theme={theme}>
+      <ReduxProvider store={store}>
+        <App />
+      </ReduxProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
