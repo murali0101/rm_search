cb7805e38c9b35d5b0dc0d17763708587de48ba7
murali0101 <muralimv0101@gmail.com>
2022-07-07 09:23:19 +0530

    Update SearchBar.jsx

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,3 +1,16 @@
+import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
+import { SearchIcon } from '@chakra-ui/icons'
+
 export const SearchBar = () => {
-  return <div>SearchBar</div>;
+  return (
+    <>
+      <InputGroup bg="#FFFFFF" maxW={450}  m={"auto"} >
+        <InputLeftElement h={55}
+          pointerEvents="none"
+          children={<SearchIcon color="gray.300" />}
+        />
+        <Input type="tel" placeholder="Search for a contact" border={0} borderRadius={0} h={55} _placeholder={{color:"gray.300"}} />
+      </InputGroup>
+    </>
+  );
 };
