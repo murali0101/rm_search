c55b45431cc1b3f934fea632fb8dd6541e2a57ff
murali0101 <muralimv0101@gmail.com>
2022-07-07 12:31:29 +0530

    Update HomePage.jsx

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -4,12 +4,14 @@ import { BasicUserCard } from "../BasicUserCard/BasicUserCard";
 import { SearchBar } from "../SearchBar/SearchBar";
 import { useDispatch, useSelector } from "react-redux";
 import { Box, Heading, VStack } from "@chakra-ui/react";
+import { DetailsUserCard } from "../DetailsUserCard/DetailsUserCard";
 
 export const HomePage = () => {
   const dispatch = useDispatch();
 
   const results = useSelector((state) => state.homePage.results);
   const info = useSelector((state) => state.homePage.info);
+ 
   console.log("results:", results);
   console.log("info:", info);
   useEffect(() => {
@@ -18,22 +20,24 @@ export const HomePage = () => {
   return (
     <>
       {results ? (
-        <Box maxW={550} m={"auto"} mt={20}>
+        <Box w="100%" m={"auto"}  >
+          <DetailsUserCard/>
           <Heading
             as="h6"
             fontSize={47}
             letterSpacing={1.5}
             textAlign="center"
-            bg="blue.400"
-          >
+            pt={20}
+            
+            >
             Rick and Morty Search
           </Heading>
           <br />
           <br />
           <SearchBar />
-          <VStack maxW={450} bg="blackAlpha.300" m={"auto"}  mt={5} textAlign="left">
+          <Box maxW={450} m={"auto"}  mt={5} textAlign="left">
             {results.map((ele, ind) => <BasicUserCard props={ ele} key={ ind} />)}
-          </VStack>
+          </Box>
         </Box>
       ) : (
         "loading....."
